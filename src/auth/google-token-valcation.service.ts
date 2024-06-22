5c125865fee60ee1c3de4539c4579e592438737c
auraofdivinity <auraofdivinity@gmail.com>
2024-06-22 11:57:50 +0530

    Adding logs

--- src/auth/google-token-valcation.service.ts
+++ src/auth/google-token-valcation.service.ts
@@ -12,16 +12,28 @@ export class GoogleTokenValidationService {
 
   async validateToken(token: string): Promise<any> {
     const clientId = this.configService.get<string>('GOOGLE_CLIENT_ID');
-    const response = await lastValueFrom(
-      this.httpService.get(
-        `https://oauth2.googleapis.com/tokeninfo?id_token=${token}`,
-      ),
-    );
+    console.log('Client ID used for validation:', clientId);
 
-    if (response.data.aud !== clientId) {
+    try {
+      const response = await lastValueFrom(
+        this.httpService.get(
+          `https://oauth2.googleapis.com/tokeninfo?id_token=${token}`,
+        ),
+      );
+
+      console.log('Token validation response:', response.data);
+
+      if (response.data.aud !== clientId) {
+        throw new UnauthorizedException('Invalid token');
+      }
+
+      return response.data;
+    } catch (error) {
+      console.error(
+        'Error validating token:',
+        error.response?.data || error.message,
+      );
       throw new UnauthorizedException('Invalid token');
     }
-
-    return response.data;
   }
 }
